0f8d5cffcaba9caa71c2053b41973167f8a31b27
Mohammad Fahad Shaik <76863803+TEGME1@users.noreply.github.com>
2022-09-23 15:25:08 +0530

    Modified login and signup responses

--- routes/authentication_routes.js
+++ routes/authentication_routes.js
@@ -7,6 +7,8 @@ const AuthenticationRoutes = express.Router()
 
 AuthenticationRoutes.post('/login', async (req, res) => {
     const { username, password } = req.body
+    console.log(req.body)
+
     try {
         const db_collection = collection(db, 'usersdata')
         const db_query = query(db_collection, where('username', '==', username))
@@ -14,37 +16,62 @@ AuthenticationRoutes.post('/login', async (req, res) => {
         const records = docs.docs.map(doc => doc.data())
 
         if (records.length === 0) {
-            res.status(404).send('User not found')
+            res.status(404).send({
+                status: 404,
+                message: 'User not found'
+            })
         } else if (records[0].password !== password) {
-            res.status(401).send('Wrong password')
+            res.status(401).send({
+                status: 401,
+                message: 'Wrong password'
+            })
         } else {
-            res.status(200).send('Logged in')
+            res.status(200).send({
+                status: 200,
+                message: 'Logged In'
+            })
         }
     }
     catch (error) {
         console.log('Error')
-        res.status(500).send('error')
+        res.status(500).send({
+            status: 500,
+            message: error
+        })
     }
 
 })
+
 AuthenticationRoutes.post('/signup', async (req, res) => {
     const { username, email, password } = req.body
+    console.log(req.body)
     try {
         const db_collection = collection(db, 'usersdata')
         const db_query = query(db_collection, where('username', '==', username))
         const docs = await getDocs(db_query)
         const records = docs.docs.map(doc => doc.data())
         if (records.length !== 0) {
-            res.status(409).send('User already exists')
+            res.status(409).send({
+                status: 409,
+                message: 'User already exists'
+            })
         }
         else {
             const addingReference = await addDoc(db_collection, req.body)
-            res.status(200).send('User added')
+            res.status(200).send({
+                status: 200,
+                message: 'User Added',
+                data: records[0]
+            })
+            console.log('User added')
         }
     }
     catch (error) {
         console.log(error)
-        res.status(500).send(error)
+        res.status(500).send({
+            status: 500,
+            message: error
+        })
     }
 })
 
